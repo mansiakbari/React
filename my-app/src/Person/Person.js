bd231c3a21672cecf920822626202faa604045f5
Mansi <mansiakbari01@gmail.com>
2022-06-29 18:20:06 +0530

    add assignment

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from "react";
+import './Person.css'
 
 const Person = (props) => {
-  return <p>I'm {props.name}..
-  i am {props.age} Years Old </p> 
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm {props.name}.. i am {props.age} Years Old{" "}
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+  )
 };
 export default Person;
